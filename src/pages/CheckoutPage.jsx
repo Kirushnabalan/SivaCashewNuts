8355a6cfe29506c893369ea9699790caf8b27b6f
kirushnabalan <kirushnabalan1803@gmail.com>
2025-06-13 09:44:32 +0530

    fix backto cart

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom" // Add Link import
 import { ArrowLeft } from "lucide-react"
 import { useCart } from "@context/CartContext"
 import { formatCurrency, isValidEmail, isValidPhone } from "@utils/helpers"
